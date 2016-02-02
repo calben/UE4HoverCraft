9507d3faee915350f4c6500a58c576b8f7635158
Calem Bendell <calem.j.bendell@gmail.com>
2016-02-02 15:57:15 -0500

    initial project files

---
+++ Source/UE4HoverCraft/UE4HoverCraft.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4HoverCraft.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4HoverCraft, "UE4HoverCraft" );
---
+++ Source/UE4HoverCraft/UE4HoverCraftGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UE4HoverCraftGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4HOVERCRAFT_API AUE4HoverCraftGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
